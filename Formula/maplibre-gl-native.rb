57613fb7ffc61d9f83530e0785cd9480c5ee608e
haluk-pointr <haluk.zorluoglu@pointr.tech>
2022-07-20 17:32:18 +0300

    Fix after rebase

--- Formula/maplibre-gl-native.rb
+++ Formula/maplibre-gl-native.rb
@@ -5,15 +5,15 @@ class MaplibreGlNative < Formula
   desc ""
   homepage "https://maplibre.org"
   license "BSD-2-Clause"
-  head "https://github.com/pointrlabs/maplibre-gl-native.git", tag:"v0.1" ,revision:"3e497161954455b9ca6845b6b4effa5f3bbf7d0a"
+  head "https://github.com/pointrlabs/maplibre-gl-native.git", tag:"v0.2" ,revision:"86e6a5d51fe9e8e525da39037690a97ac1dd896d"
 
   depends_on "cmake" => [:build, "3.0"]
   depends_on "qt5" => "5.15.3"
-  #depends_on "ninja" => :build
+  depends_on "ninja" => :build
 
   def install
-    system "cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{prefix} -DMBGL_WITH_QT=ON -DMBGL_QT_LIBRARY_ONLY=ON -DMBGL_WITH_WERROR=OFF"
-    system "cmake --build build --target qmapboxgl"
+    system "cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{prefix} -DMBGL_WITH_QT=ON -DMBGL_QT_LIBRARY_ONLY=ON"
+    system "cmake --build build --target qmaplibregl"
     system "cmake --install build"
   end
 end
