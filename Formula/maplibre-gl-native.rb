ef70a32b7fc31736ba71f365b512fbbb1f84ceb7
Can Tunca <cantunca@gmail.com>
2023-02-22 16:58:49 +0300

    Update maplibre-gl-native version to v0.5
    Add linux support

--- Formula/maplibre-gl-native.rb
+++ Formula/maplibre-gl-native.rb
@@ -6,14 +6,21 @@ class MaplibreGlNative < Formula
   homepage "https://maplibre.org"
   license "BSD-2-Clause"
   head "https://github.com/pointrlabs/maplibre-gl-native.git", branch:"develop"
-  url "https://github.com/pointrlabs/maplibre-gl-native.git", tag:"v0.4", revision:"613485188f5bb92a438a48ee7af16eb25a55e6dd"
+  url "https://github.com/pointrlabs/maplibre-gl-native.git", tag:"v0.5", revision:"309d9eed1f3cacb2a82d6cb4bee031965c215cde"
 
   depends_on "cmake" => [:build, "3.16"]
   depends_on "ninja" => [:build, "1.10.2"]
+  on_linux do
+    depends_on "llvm" => :build
+  end
   depends_on "qt6" => "6.0.1"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_QT=ON", "-DMBGL_QT_LIBRARY_ONLY=ON", *std_cmake_args
+    if OS.linux?
+      system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_QT=ON", "-DMBGL_QT_LIBRARY_ONLY=ON", "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++", "-DMBGL_WITH_WERROR=OFF", "-DMBGL_QT_WITH_INTERNAL_SQLITE=ON", *std_cmake_args
+    else
+      system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_QT=ON", "-DMBGL_QT_LIBRARY_ONLY=ON", *std_cmake_args
+    end
     system "cmake", "--build", "build", "--target", "qmaplibregl"
     system "cmake", "--install", "build"
   end
