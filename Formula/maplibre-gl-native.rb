21e8ac1dba8cfe99921233191c10b2df1e0665e4
Can Tunca <cantunca@gmail.com>
2023-05-17 16:25:52 +0300

    Fix audit issues

--- Formula/maplibre-gl-native.rb
+++ Formula/maplibre-gl-native.rb
@@ -1,5 +1,5 @@
 class MaplibreGlNative < Formula
-  desc "Maplibre GL Native (Pointr-flavored)"
+  desc "Pointr-flavored Maplibre GL Native"
   homepage "https://maplibre.org"
   url "https://github.com/pointrlabs/maplibre-gl-native.git", tag: "v0.5", revision: "309d9eed1f3cacb2a82d6cb4bee031965c215cde"
   license "BSD-2-Clause"
@@ -7,13 +7,14 @@ class MaplibreGlNative < Formula
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
+  depends_on "qt6"
   on_linux do
     depends_on "llvm" => :build
   end
-  depends_on "qt6"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_QT=ON", "-DMBGL_QT_LIBRARY_ONLY=ON", "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++", "-DMBGL_WITH_WERROR=OFF", "-DMBGL_QT_WITH_INTERNAL_SQLITE=ON", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_QT=ON", "-DMBGL_QT_LIBRARY_ONLY=ON", \
+      "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++", "-DMBGL_WITH_WERROR=OFF", "-DMBGL_QT_WITH_INTERNAL_SQLITE=ON", *std_cmake_args
     system "cmake", "--build", "build", "--target", "qmaplibregl"
     system "cmake", "--install", "build"
   end
