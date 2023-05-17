a3b8e5054c7e7bdf100f8b61e4ce1691b1081af1
Can Tunca <cantunca@gmail.com>
2023-05-17 16:20:28 +0300

    Fix audit issues

--- Formula/maplibre-gl-native.rb
+++ Formula/maplibre-gl-native.rb
@@ -1,19 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class MaplibreGlNative < Formula
-  desc ""
+  desc "Maplibre GL Native (Pointr-flavored)"
   homepage "https://maplibre.org"
+  url "https://github.com/pointrlabs/maplibre-gl-native.git", tag: "v0.5", revision: "309d9eed1f3cacb2a82d6cb4bee031965c215cde"
   license "BSD-2-Clause"
-  head "https://github.com/pointrlabs/maplibre-gl-native.git", branch:"develop"
-  url "https://github.com/pointrlabs/maplibre-gl-native.git", tag:"v0.5", revision:"309d9eed1f3cacb2a82d6cb4bee031965c215cde"
+  head "https://github.com/pointrlabs/maplibre-gl-native.git", branch: "develop"
 
-  depends_on "cmake" => [:build, "3.16"]
-  depends_on "ninja" => [:build, "1.10.2"]
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
   on_linux do
     depends_on "llvm" => :build
   end
-  depends_on "qt6" => "6.0.1"
+  depends_on "qt6"
 
   def install
     system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_QT=ON", "-DMBGL_QT_LIBRARY_ONLY=ON", "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++", "-DMBGL_WITH_WERROR=OFF", "-DMBGL_QT_WITH_INTERNAL_SQLITE=ON", *std_cmake_args
--- Formula/maplibre-map-renderer.rb
+++ Formula/maplibre-map-renderer.rb
@@ -1,15 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class MaplibreMapRenderer < Formula
-  desc ""
+  desc "Produces raster images from Maplibre maps"
   homepage "https://github.com/pointrlabs/maplibre-gl-native/tree/develop/map-renderer"
+  url "https://github.com/pointrlabs/maplibre-gl-native.git", tag: "version/map-renderer/1.0", revision: "30434314e43ae0145a2c67ccbd6b07f1229f4c5b"
   license "MIT"
-  head "https://github.com/pointrlabs/maplibre-gl-native.git", branch:"develop"
-  url "https://github.com/pointrlabs/maplibre-gl-native.git", tag:"version/map-renderer/1.0", revision:"30434314e43ae0145a2c67ccbd6b07f1229f4c5b"
+  head "https://github.com/pointrlabs/maplibre-gl-native.git", branch: "develop"
 
-  depends_on "cmake" => [:build, "3.16"]
-  depends_on "ninja" => [:build, "1.10.2"]
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
 
   def install
     system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_MAP_RENDERER=ON", *std_cmake_args
