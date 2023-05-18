252b05d75b632fabf3d5b6965d69c1d10ed7c7c6
Can Tunca <can.tunca@pointr.tech>
2023-05-18 14:14:43 +0300

    Revise formulae specs for workflow compatibility
    Closes #4.
    
    Signed-off-by: cantunca-pointr <36108266+cantunca-pointr@users.noreply.github.com>

--- Formula/maplibre-map-renderer.rb
+++ Formula/maplibre-map-renderer.rb
@@ -1,5 +1,5 @@
 class MaplibreMapRenderer < Formula
-  desc "Produces raster images from Maplibre maps"
+  desc "Renders and produces raster images from Maplibre maps"
   homepage "https://github.com/pointrlabs/maplibre-gl-native/tree/develop/map-renderer"
   url "https://github.com/pointrlabs/maplibre-gl-native.git", tag: "version/map-renderer/1.0", revision: "30434314e43ae0145a2c67ccbd6b07f1229f4c5b"
   license "MIT"
@@ -7,9 +7,17 @@ class MaplibreMapRenderer < Formula
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
+  uses_from_macos "curl"
+  on_linux do
+    depends_on "libuv"
+    depends_on "mesa-glu"
+    depends_on "mesalib-glw"
+  end
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", "-DMBGL_WITH_MAP_RENDERER=ON", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
+      "-DMBGL_WITH_MAP_RENDERER=ON", "-DMBGL_WITH_WERROR=OFF",
+      *std_cmake_args
     system "cmake", "--build", "build", "--target", "map-renderer"
     system "cmake", "--install", "build"
   end
