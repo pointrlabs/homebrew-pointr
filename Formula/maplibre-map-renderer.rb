b7ccbdd8c7e690177479194629c930e6cb7bea20
Can Tunca <cantunca@gmail.com>
2023-05-24 12:22:23 +0300

    [maplibre-map-renderer] Add bottle for monterey

--- Formula/maplibre-map-renderer.rb
+++ Formula/maplibre-map-renderer.rb
@@ -8,7 +8,8 @@ class MaplibreMapRenderer < Formula
   bottle do
     root_url "https://github.com/pointrlabs/homebrew-vendor/releases/download/maplibre-map-renderer-1.0"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, ventura: "8788c2cf89b6e39595cc7a448cd7e19becd2be8bba90f0428611e237245b4ad1"
+    sha256 cellar: :any_skip_relocation, ventura:  "8788c2cf89b6e39595cc7a448cd7e19becd2be8bba90f0428611e237245b4ad1"
+    sha256 cellar: :any_skip_relocation, monterey: "7907347257e3a34bdd95917278472b9efa40ad783fcb39931b610dce2794a643"
   end
 
   depends_on "cmake" => :build
