7f3d33b9ae4cfde2f8991cb217205d56b60c6360
Can Tunca <can.tunca@pointr.tech>
2023-05-26 18:03:34 +0300

    Revise workflows

--- Formula/maplibre-gl-native.rb
+++ Formula/maplibre-gl-native.rb
@@ -8,6 +8,7 @@ class MaplibreGlNative < Formula
   bottle do
     root_url "https://github.com/pointrlabs/homebrew-vendor/releases/download/maplibre-gl-native-0.5"
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "ede61abf8558e242b2ed5efe1acd54586038de11d868b8b10ac411fc88b1d81c"
     sha256 cellar: :any,                 arm64_monterey: "0933d9f23f44a7659348954d4ac9f46b3d61611acb018a1b937744ccc07d35c8"
     sha256 cellar: :any,                 ventura:        "b09808287c3e7b1b7d30f3bbfbfd465430ecab5d0a02466fb0915ec856ab4de0"
     sha256 cellar: :any,                 monterey:       "ec52135fa73f2b29c2e33e026394b2f61925efa1ab067fced617e219a1cd060a"
